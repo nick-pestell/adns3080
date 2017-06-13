2a4b8fa0c7c5012c5c64f59d72a8b841eeb1bb17
Nick Pestell <np0877@bristol.ac.uk>
2017-06-13 16:05:25 +0100

    give image quality instead of dx & dy

--- examples/motionData/motionData.ino
+++ examples/motionData/motionData.ino
@@ -10,18 +10,24 @@ void setup() {
   ADNS_3080.powerUp();
 }
 
-
+unsigned long Time = 0;
+int squal;
+int mot;
 void loop() {
   
   burst_data data;
   ADNS_3080.burstRead(&data);
-  int mot = (int)data.motion;
-  if(mot == 129){
-    int x = ADNS_3080.convTwosComp(data.dx);
-    int y = ADNS_3080.convTwosComp(data.dy); 
-    Serial.print("dx: ");
-    Serial.print( x );
-    Serial.print(" dy: ");
-    Serial.println( y ); 
-   }
+  //check if movement
+  //mot = (int)data.motion;
+  //if(mot == 129){
+    //int x = ADNS_3080.convTwosComp(data.dx);
+    //int y = ADNS_3080.convTwosComp(data.dy); 
+    
+    //Serial.print("dx: ");
+    //Serial.print( x );
+    //Serial.print(" dy: ");
+    //Serial.println( y ); 
+ //  }
+ squal = (int)data.squal;
+ Serial.println(squal); 
 }
