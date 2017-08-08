ab628145993364c4678043c572083c51bd4ebd9e
nick_pestell <np0877@bristol.ac.uk>
2017-08-08 20:07:19 +0100

    add painThreshold.ino

---
+++ examples/painThreshhold/painThreshhold.ino
@@ -0,0 +1,30 @@
+#include <adns3080.h>
+#include <SPI.h>
+
+adns3080 ADNS_3080(4,10);
+
+void setup() {
+  Serial.begin(115200);
+  ADNS_3080.initSPI();
+  ADNS_3080.powerUp();
+
+  pinMode(2,OUTPUT);
+  digitalWrite(2,HIGH);
+
+  
+  //ADNS_3080.writeTo(FRAME_PERIOD_MAXB_LOWER_REG,0x7E);
+  //ADNS_3080.writeTo(FRAME_PERIOD_MAXB_UP_REG,0xE);
+
+}
+
+byte SQUAL;
+
+void loop() {  
+  
+  //Serial.println(ADNS_3080.readFrom(SQUAL_REG,1));
+  if (ADNS_3080.readFrom(SQUAL_REG,1) < 0x64){
+    Serial.print("1");
+
+  }
+  
+}
