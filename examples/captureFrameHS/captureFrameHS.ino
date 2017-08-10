a75b3cb7455d77986f281b2f5a953f58b80449bd
Nick <Nick>
2017-08-10 22:21:00 +0100

    add captureFrameHS for robust frame capture in matlab

---
+++ examples/captureFrameHS/captureFrameHS.ino
@@ -0,0 +1,21 @@
+#include <adns3080.h>
+
+adns3080 ADNS_3080(4,10);
+byte frame_data[30][30];
+
+void setup() {
+
+  pinMode(2,OUTPUT);
+  digitalWrite(2,HIGH);
+  
+  Serial.begin(115200);
+  ADNS_3080.initSPI();
+  ADNS_3080.powerUp();
+}
+
+
+void loop() {
+
+  ADNS_3080.captureFrame(frame_data);
+  
+}
